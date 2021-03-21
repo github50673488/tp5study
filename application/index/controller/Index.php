4c3aeac5b226d0afa68fa92f02b129ff5d0fb446
liuguojun <gj.liu@sj-sol.com>
2021-03-21 14:35:17 +0900

    （9）视图 https://www.kancloud.cn/thinkphp/thinkphp5_quickstart/478276

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,18 @@
 <?php
 namespace app\index\controller;
 
-class Index
+use think\Controller;
+
+class Index extends Controller
 {
     public function index()//http://localhost/tp5study/
     {
         return 'hello world liu';
+    }
+
+    public function hello($name = 'thinkphp')//http://tp5study.com/index/index/hello
+    {
+        $this->assign('name', $name);
+        return $this->fetch();//fetch方法中我们没有指定任何模板，所以按照系统默认的规则（视图目录/控制器/操作方法）输出了view/index/hello.html模板文件。
     }
 }
