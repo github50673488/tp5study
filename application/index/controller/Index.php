5d7e0ef191617293b889ad88cbaabd3cedb54140
liuguojun <gj.liu@sj-sol.com>
2021-03-21 15:15:07 +0900

    （2）参数传入

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -18,4 +18,11 @@ class Index extends Controller
         $this->assign('name', $name);
         return $this->fetch();//fetch方法中我们没有指定任何模板，所以按照系统默认的规则（视图目录/控制器/操作方法）输出了view/index/hello.html模板文件。
     }
+
+    public function hello1($name = 'World', $city = '')
+        //http://tp5study.com/index/index/hello1?city=shanghai&name=thinkphp
+        // or http://tp5study.com/index.php/index/index/hello1/city/shanghai/name/thinkphp
+    {
+        return 'Hello,' . $name . '! You come from ' . $city . '.';
+    }
 }
